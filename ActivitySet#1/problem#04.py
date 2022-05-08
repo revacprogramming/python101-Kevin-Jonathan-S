18ab00186344956a3523b10106765af6c0eac978
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-05-08 17:22:01 +0000

    Programmed Activity Set 1, Program 4, have already done this before, but in a wrong repl file

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,17 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+hrs = input("Enter hours: ")
+rate = input("Enter rate per hour: ")
+try:
+  Working = float(hrs)
+  PPW = float(rate)
+except:
+  print("Enter in numeric form :(")
+  quit()
+if Working > 40:
+  bonusrate = PPW * 1.5
+  bonuspay = (Working - 40) * bonusrate
+  pay = ((Working - (Working - 40)) * PPW) + bonuspay
+else:
+  pay = Working * PPW
+print("Your pay is: $", pay)
