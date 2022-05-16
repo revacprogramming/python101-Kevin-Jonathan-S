dd1a2c372be81d860e039db3772fabeefd6e48fa
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-05-16 06:19:03 +0000

    Programmed Activity Set 1, Program 8 and 9

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,8 @@
 # Files
 
 filename = "dataset/mbox-short.txt"
+
+fh = open(filename)
+for lx in fh:
+  ly=lx.rstrip()
+  print(ly.upper())
--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,11 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+han = open('dataset/romeo.txt')
+
+for line in han:
+  line = line.rstrip()
+  wds = line.split()
+  if len(wds) < 3 or wds[0] != 'From':
+    continue
+  print(wds[2])
