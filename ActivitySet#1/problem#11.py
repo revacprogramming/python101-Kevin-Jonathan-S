45276e6a7b721ea643f516a84dbbf11fdcc1c3e7
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-05-26 09:34:00 +0000

    Programmed Activity Set 1, Program 11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,20 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+han = open('dataset/romeo.txt')
+
+for line in han:
+  line = line.rstrip()
+  wds = line.split()
+  for w in wds:
+    di[w] = di.get(w,0) + 1
+
+tmp = list()
+for k,v i di.items() :
+  newt = (v,k)
+  tmp.append(newt)
+
+tmp = sorted(tmp, reverse=True)
+
+for v,k in tmp[:5]:
+  print(k,v)
