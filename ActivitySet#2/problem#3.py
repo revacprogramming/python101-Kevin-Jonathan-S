17b9481dab1d9b8e1cc761d9f4a0658418e6e8e3
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-05-26 09:46:36 +0000

    Programmed Activity Set 2, Program 3

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,17 +1,26 @@
-
-
 def get_cs():
-    """get string input"""
+  s=int(input("how many string do you need?"))
+  l=[]
+  for i in range(0,s):
+    s=input("enter string")
+    l.append(s)
+  return l
 
 
-def cs_to_lot(cs):
-    """convert connected string to list of strings"""
+def cs_to_lot(l):
+  n=[]
+  for i in l:
+    n.append((i,i[0]))
+
+  return n
+    
+    
 
 
 def main():
-    cs = get_cs()
+    l = get_cs()
 
-    lot = cs_to_lot(cs)
+    lot = cs_to_lot(l)
     print(lot)
 
 
