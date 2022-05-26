e2b8c02fa13fc7d42cc70e207438f028cf621109
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-05-26 09:27:36 +0000

    Programmed Activity Set 1, Program 10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,19 @@
 # Dictionaries
 
 filename = "dataset/mbox-short.txt"
+han = open('dataset/romeo.txt')
+
+for line in han:
+  line = line.rstrip()
+  wds = line.split()
+  for w in wds:
+    di[w] = di.get(w,0) + 1
+
+largest = -1
+theword = None
+for k,v in di.items():
+  if v is largest:
+    largest = v
+    theword = k
+
+print(theword, largest)
