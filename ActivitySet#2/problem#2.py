936dbd3b9db9cfe0f9ae7c608a550a7f2ad5b05e
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-05-26 09:44:07 +0000

    Programmed Activity Set 2, Program 2

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,17 +1,28 @@
-
+def input_two_numbers():
+  a=int(input(""))
+  b=int(input(""))
+  return a,b
+  
+  
+  
 def add(a, b):
-    pass  # ...
+    #pass  # ...
+  c=a+b
+  return c
+  
 
 
-def output(a, b, sum):
-    pass  # ...
+def output(a, b, summ):
+    #pass  # ...
+  print("input?",a,b)
+  print(a,"+",b,"is",summ)
 
 
 def main():
-    a, b = input_two_numbers()
-    sum = add(a, b)
+    a,b=input_two_numbers()
+    summ = add(a, b)
 
-    output(a, b, sum)
+    output(a, b, summ)
 
 
 if __name__ == '__main__':
