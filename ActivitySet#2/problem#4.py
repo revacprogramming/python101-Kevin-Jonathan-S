8c2fc6d8a5d987d5a00b0631e7b8da68b8c34e33
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-06-02 10:24:28 +0000

    Programmed Activity Set 2, Program 4

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,15 +1,26 @@
-
-
 def get_cs():
-    """get string input"""
+  s=int(input("how many string do you need?"))
+  l=[]
+  for i in range(0,s):
+    s=input("enter string")
+    l.append(s)
+  return l
 
 
 def cs_to_lot(cs):
-    """convert connected string to list of strings"""
+  """convert connected string to list of strings"""
+  n=[]
+  for i in cs:
+    n.append((i,i[0]))
+  return n
 
 
 def lot_to_cs(lot):
-    """convert list of strings to connected string"""
+  """convert list of strings to connected string"""
+  for i in lot:
+    print(i[0],"=",i[1],end = "  ")
+  print()
+
 
 
 def main():
@@ -19,7 +30,6 @@ def main():
     print(lot)
 
     cs=lot_to_cs(lot)  # convert list of strings to connect string
-    print(cs)
 
 
 if __name__ == '__main__':
