b63bd3e55713c1205b10b27e7882558d22e489e7
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-06-07 17:50:38 +0000

    Re-programming Activity Set 1 Program 4

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,5 +1,16 @@
 # Conditional Execution
 
+def compute(Working, PPW):
+  if Working > 40:
+    bonusrate = PPW * 1.5
+    bonuspay = (Working - 40) * bonusrate
+    pay = ((Working - (Working - 40)) * PPW) + bonuspay
+  else:
+    pay = Working * PPW
+    print("Your pay is: $", pay)
+    return pay
+
+
 hrs = input("Enter hours: ")
 rate = input("Enter rate per hour: ")
 try:
