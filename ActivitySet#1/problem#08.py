8f490d6c63efd16ea841cd77a2e228102e52de8c
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-07-04 13:55:28 +0000

    Re-programmed Activity Set 1, Program 8

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,8 +1,28 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+def in_put():
+    file_name = input("Enter name of file: ")
+    return file_name
 
-fh = open(filename)
-for lx in fh:
-  ly=lx.rstrip()
-  print(ly.upper())
+def open_file(file_name) :
+
+    if file_name == 'progrm8.txt':
+        file_exe = open('progrm8.txt')
+    else:
+        print("Invalid file name")
+    return file_exe
+
+def convert_upper(file_exe) :
+
+    for lines in file_exe:
+        capitalized = lines.upper()
+        print(capitalized)
+    
+def main() :
+
+    file_name = in_put()
+    file_exe = open_file(file_name)
+    convert_upper(file_exe)
+
+if __name__=='__main__' :
+    main()
