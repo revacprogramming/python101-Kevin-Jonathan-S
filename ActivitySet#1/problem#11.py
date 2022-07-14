962e6b92ac6060aeb80637aa159199b87bf5a156
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-07-14 12:16:28 +0000

    Re-programmed Activity Set 1, Program 10

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,20 +1,21 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
-han = open('dataset/romeo.txt')
+filehandle=open(filename)
+counts={}
 
-for line in han:
-  line = line.rstrip()
-  wds = line.split()
-  for w in wds:
-    di[w] = di.get(w,0) + 1
+for line in filehandle:
+	if line.startswith("From "):
+		time=line.split()[5].split(":")
+		counts [time[0]] = counts.get(time[0], 0) + 1
 
-tmp = list()
-for k,v i di.items() :
-  newt = (v,k)
-  tmp.append(newt)
 
-tmp = sorted(tmp, reverse=True)
+lst = list()
 
-for v,k in tmp[:5]:
-  print(k,v)
+for key, value in counts.items():
+    lst.append( (key,value) )
+lst.sort()
+
+for hour, counts in lst:
+    print (hour, counts)   
+  
